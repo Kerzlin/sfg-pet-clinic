11032da9f94aca24d8564f695f734f839362db3d
kerzlin <natalija.kerzlin@gmail.com>
2020-03-23 17:50:48 +0100

    Implement Map Based Services. closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
+
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
@@ -20,6 +17,14 @@ public class BaseEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 
   public boolean isNew() {
     return this.id == null;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -49,7 +49,7 @@ public class Owner extends Person {
    * @return true if pet name is already in use
    */
   public Pet getPet(String name) {
-    return getPet(name, false);
+    return null;// getPet(name, false);
   }
 
   /**
@@ -58,18 +58,18 @@ public class Owner extends Person {
    * @param name to test
    * @return true if pet name is already in use
    */
-  public Pet getPet(String name, boolean ignoreNew) {
-    name = name.toLowerCase();
-    for (Pet pet : pets) {
-      if (!ignoreNew || !pet.isNew()) {
-        String compName = pet.getName();
-        compName = compName.toLowerCase();
-        if (compName.equals(name)) {
-          return pet;
-        }
-      }
-    }
-    return null;
-  }
+//  public Pet getPet(String name, boolean ignoreNew) {
+//    name = name.toLowerCase();
+//    for (Pet pet : pets) {
+//      if (!ignoreNew || !pet.isNew()) {
+//        String compName = pet.getName();
+//        compName = compName.toLowerCase();
+//        if (compName.equals(name)) {
+//          return pet;
+//        }
+//      }
+//    }
+//    return null;
+//  }
 
 }
