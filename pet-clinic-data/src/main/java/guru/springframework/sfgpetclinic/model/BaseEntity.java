aec3aa69106bc96d540cb332c7fc046172fe3212
kerzlin <natalija.kerzlin@gmail.com>
2020-09-08 10:09:36 +0200

    Updated Base netity with JPA. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,14 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 //@NoArgsConstructor
 //@AllArgsConstructor
-//@MappedSuperclass
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
- // @Id
- // @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
 //  public BaseEntity(Long id) {
