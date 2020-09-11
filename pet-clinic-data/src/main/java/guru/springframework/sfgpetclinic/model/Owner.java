e83dc82ce9a33a11d760abc39b36067dd9a45bfc
kerzlin <natalija.kerzlin@gmail.com>
2020-09-11 16:19:48 +0200

    Lombok. Closes #52.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,29 +1,34 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-//@Setter
-//@Getter
-//@NoArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
 
 
-//  @Builder
-//  public Owner(Long id, String firstName, String lastName, String address, String city,
-//               String telephone, Set<Pet> pets) {
-//    super(id, firstName, lastName);
-//    this.address = address;
-//    this.city = city;
-//    this.telephone = telephone;
-//
-//    if(pets != null) {
-//      this.pets = pets;
-//    }
-//  }
+  @Builder
+  public Owner(Long id, String firstName, String lastName, String address, String city,
+               String telephone, Set<Pet> pets) {
+    super(id, firstName, lastName);
+    this.address = address;
+    this.city = city;
+    this.telephone = telephone;
+
+    if(pets != null) {
+      this.pets = pets;
+    }
+  }
 
   @Column(name = "address")
   private String address;
@@ -36,38 +41,6 @@ public class Owner extends Person {
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getCity() {
-    return city;
-  }
-
-  public void setCity(String city) {
-    this.city = city;
-  }
-
-  public String getTelephone() {
-    return telephone;
-  }
-
-  public void setTelephone(String telephone) {
-    this.telephone = telephone;
-  }
-
-  public Set<Pet> getPets() {
-    return pets;
-  }
-
-  public void setPets(Set<Pet> pets) {
-    this.pets = pets;
-  }
 
 /**
    * Return the Pet with the given name, or null if none found for this Owner.
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,19 +1,26 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotEmpty;
 
-//@AllArgsConstructor
-//@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter // klase koje se inherituju najbolje dodate geter i seter
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-//  public Person(Long id, String firstName, String lastName) {
-//    super(id);
-//    this.firstName = firstName;
-//    this.lastName = lastName;
-//  }
+  public Person(Long id, String firstName, String lastName) {
+    super(id);
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
   @Column(name = "first_name")
   @NotEmpty
@@ -23,19 +30,4 @@ public class Person extends BaseEntity {
  @NotEmpty
   private String lastName;
 
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
 }
