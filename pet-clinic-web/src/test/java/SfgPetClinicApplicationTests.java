825d43a4d945ce4330aaf5df4e736c641ec96081
kerzlin <natalija.kerzlin@gmail.com>
2020-10-08 16:17:59 +0200

    SfgPetClinicApplicationTest

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -1,11 +1,12 @@
 
+import com.sun.glass.ui.Application;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class SfgPetClinicApplicationTests {
 
     @Test
