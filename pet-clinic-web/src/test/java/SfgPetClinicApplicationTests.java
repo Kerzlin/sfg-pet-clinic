032206e35c4009359339a52922336942b7866cc6
kerzlin <natalija.kerzlin@gmail.com>
2020-10-08 13:53:07 +0200

    Adding import org.junit.jupiter.api.Test; to SfgPetClinicApplicationTest

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -1,6 +1,5 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
